c645f251b99d4e1474d3defecd4605c469ecdafd
Ray Wang <raywang@nvidia.com>
2025-12-07 22:49:13 -0800

    Add next_n=4 for SM90

--- csrc/jit_kernels/impls/smxx_fp8_paged_mqa_logits.hpp
+++ csrc/jit_kernels/impls/smxx_fp8_paged_mqa_logits.hpp
@@ -204,8 +204,9 @@ static void smxx_fp8_paged_mqa_logits(const torch::Tensor& q,
         const int aligned_smem_weight_size_per_stage = align(next_n * num_heads * static_cast<int>(weights.element_size()), swizzle_alignment);
         const int smem_q_pipe_size = num_q_stages * (smem_q_size_per_stage + aligned_smem_weight_size_per_stage) + align(num_q_stages * 8 * 2, swizzle_alignment);
 
-        const int smem_kv_size_per_stage = block_kv * head_dim * static_cast<int>(kv_cache.element_size());
-        const int aligned_smem_kv_scale_size_per_stage = align(block_kv * static_cast<int>(kv_cache_scales.element_size()), swizzle_alignment);
+        const int compute_block_kv = 64;
+        const int smem_kv_size_per_stage = compute_block_kv * head_dim * static_cast<int>(kv_cache.element_size());
+        const int aligned_smem_kv_scale_size_per_stage = align(compute_block_kv * static_cast<int>(kv_cache_scales.element_size()), swizzle_alignment);
         const int smem_kv_pipe_size = num_kv_stages * (smem_kv_size_per_stage + aligned_smem_kv_scale_size_per_stage) + align(num_kv_stages * 8 * 2, swizzle_alignment);
 
         // Allocate some shared memory for UMMA barriers and tensor memory pointer, although it is not used in SM90
